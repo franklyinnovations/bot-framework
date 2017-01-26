30ccb8f6c97ffc28d6bd718e5cc2c88bb43e381f
Adam Juhasz <adam@akitram.com>
2017-01-25 18:24:52 -0800

    removing platforms

--- lib/platforms/index.d.ts
+++ lib/platforms/index.d.ts
@@ -1,5 +1,3 @@
-import Console from './shell';
 import Facebook from './facebook';
 import Web from './web';
-import TestPlatform from './test';
-export { Console, Facebook, Web, TestPlatform };
+export { Facebook, Web };
